09a58e730bf88e76c5bc5fd1ca7343ffdd861ecb
Nadil Saad <nadilsaad17@gmail.com>
2026-04-09 13:58:34 -0700

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
+def home(request):
+    return JsonResponse({"message": "SME Business Management System API is running"})
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include("business.urls")),
 ]
